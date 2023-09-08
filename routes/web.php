89fda48256b9fdee45c61f2ea98e77f540ec2ecf
timvandewalle <tim@vandewalle.mobi>
2023-09-08 21:39:12 +0200

    Add routes for footer pages + login procedure

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('login', [AuthController::class, 'showLogin'])->name('login.show');
+Route::get('login/{provider}', [AuthController::class, 'redirectToProvider'])->where('provider', 'google|apple');
+Route::get('login/{provider}/callback', [AuthController::class, 'handleProviderCallback'])->where('provider', 'google|apple');
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::resource('perfumes', PerfumeController::class);
 Route::resource('symptoms', SymptomController::class);
+
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/privacy', function () {
+    return view('privacy');
+})->name('privacy');
+
+Route::get('/terms', function () {
+    return view('terms');
+})->name('terms');

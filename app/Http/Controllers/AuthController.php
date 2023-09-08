253cf77930b6409b8a969ba3ef5814a808a04525
timvandewalle <tim@vandewalle.mobi>
2023-09-08 21:38:48 +0200

    Add sociale login (part 1)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
-    //
-}
+    public function showLogin()
+    {
+        return view('login');  // This assumes you have a login.blade.php in your views folder
+    }
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+
+        // Perform actions like logging in the user or creating a user in your database
+        // based on the provider.
+
+        // Here, you can also check for the 'provider' and 'provider_id' in the database.
+        // If a user with the same 'provider' and 'provider_id' exists, log them in.
+        // Otherwise, create a new user.
+    }}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+    <div class="container mx-auto text-center py-16">
+        <h1 class="text-darkblue text-5xl font-bold mb-6">Login to PerfumePicker</h1>
+        <p class="text-darkgrey mb-6">Choose your login method:</p>
+
+        <div class="space-y-4">
+            <a href="{{ url('login/google') }}" class="bg-lightblue text-darkgrey px-6 py-2 rounded hover:bg-mint">
+                <i class="fab fa-google"></i> Login with Google
+            </a>
+
+            <a href="{{ url('login/apple') }}" class="bg-lightgrey text-darkgrey px-6 py-2 rounded hover:bg-mint">
+                <i class="fab fa-apple"></i> Login with Apple
+            </a>
+        </div>
+    </div>
+@endsection
